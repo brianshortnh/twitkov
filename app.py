56644d8d1b04fea364ddf18a69c0cd2d3cffe020
Matt Lewis <domattthings@gmail.com>
2018-01-23 21:29:58 -0500

    Markov chain for Trump

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 """Rough sample app of Markov Generator"""
 import twitter
 import os
+import markovify
 
+def generate(stuff):
+    # Build the model.
+    text_model = markovify.Text(" ".join(stuff))
+
+    # Print five randomly-generated sentences
+    for i in range(5):
+        print(text_model.make_sentence())
+
+    # Print three randomly-generated sentences of no more than 140 characters
+    for i in range(88):
+        print(text_model.make_short_sentence(140))
 
 def get_all_tweets(username):
     """Spawns api and gets last 2000 tweets
@@ -11,7 +23,7 @@ def get_all_tweets(username):
     api = new_api()
     tweets = get_tweets(api, username)
 
-    for i in range(0, 10):
+    for i in range(0, 300):
         tweets += get_tweets(api, username, since=tweets[len(tweets)-1].id)
 
     return tweets
@@ -35,5 +47,5 @@ def get_tweets(api, username, since=None):
         max_id=since)
 
 if __name__ == '__main__':
-    data = [tweet.text for tweet in get_all_tweets("dancinpolkabear")]
-    print(data)
+    data = [tweet.text for tweet in get_all_tweets("realDonaldTrump")]
+    generate(data)
