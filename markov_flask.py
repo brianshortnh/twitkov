b611f2001cbf21e235b552ca8c887b2bc9a0af93
Brian Short <brianshortnh@comcast.net>
2018-01-25 00:30:15 -0500

    Changed filenames and added a .gitignore

--- markov_flask.py
+++ markov_flask.py
@@ -1,13 +1,13 @@
 #Flask quickstart
-import nathan
+import markov_app as mkv
 from flask import Flask, request, json
 app = Flask(__name__)
 
 @app.route('/<twitter_handle>')
 #def make_one_tweet(twitter_handle):
-#    tweet = nathan.make_tweet_string(twitter_handle, 1)
+#    tweet = mkv.make_tweet_string(twitter_handle, 1)
 #    return "New tweet for {0}:\n{1}".format(twitter_handle, tweet)
 
 def make_five_tweets(twitter_handle):
-    tweets = nathan.make_tweet_string(twitter_handle, 5)
+    tweets = mkv.make_tweet_string(twitter_handle, 5)
     return "New tweets for {0}:<br/>{1}".format(twitter_handle, tweets)
