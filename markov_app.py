99f0b37d9a3544a4fa7df7422461855a9b833dba
Matt Lewis <domattthings@gmail.com>
2018-01-28 22:42:51 -0500

    Refactor tweet generation

--- markov_app.py
+++ markov_app.py
@@ -1,37 +1,42 @@
 """Rough sample app of Markov Generator"""
-import twitter
 import os
+import twitter
 import markovify
 
-def generate(tweets, bound):
-    generated = []
+TWEET_LIMIT = 200
 
-    # Build the model.
-    text_model = markovify.Text(" ".join(tweets))
-
-    # Create a list ofgenerated tweets
-    for i in range(0, bound):
-        new_tweet = text_model.make_short_sentence(140)
-        generated.append(new_tweet)
-
-    return generated
+def generate(text_model, size, bound):
+    """Makes 140 character tweets"""
+    return [text_model.make_short_sentence(size) for i in range(0, bound)]
 
 def get_all_tweets(username):
-    """Spawns api and gets last 2000 tweets
-
-    @todo check rate limit
-    """
+    """Spawns api and gets last 2000 tweets"""
     api = new_api()
     tweets = get_tweets(api, username)
 
-    for i in range(0, 10):
+    for i in range(0, 20):
         tweets += get_tweets(api, username, since=tweets[len(tweets)-1].id)
-#Debug
-        print('poop ' + str(i))
-        print(username)
 
     return tweets
 
+def make_tweets(username, num_tweets):
+    """Produce an array of generated tweets"""
+    data = [tweet.text for tweet in get_all_tweets(username)]
+    model = make_markov_model(data)
+
+    return {
+        'username': username,
+        'tweets': generate(model, 140, num_tweets),
+        'long': generate(model, 240, 2),
+    }
+
+def make_tweet_string(username, num_tweets):
+    """Produce a valid string of generated tweets"""
+    data = [tweet.text for tweet in get_all_tweets(username)]
+    tweet_string = "<br/>#############<br/>".join(generate(data, 140, num_tweets))
+    return tweet_string
+
+# Utility
 def new_api():
     """Wrapper around spawning twitter api"""
     return twitter.Api(
@@ -44,28 +49,15 @@ def get_tweets(api, username, since=None):
     """Wrapper around api request"""
     return api.GetUserTimeline(
         screen_name=username,
-        count=200,
+        count=TWEET_LIMIT,
         include_rts=False,
         trim_user=False,
         exclude_replies=True,
         max_id=since)
 
-def make_tweets(username, num_tweets):
-    """Produce an array of generated tweets"""
-    data = [tweet.text for tweet in get_all_tweets(username)]
-    tweets = generate(data, num_tweets)
-
-    return {
-        'username': username,
-        'tweets': tweets
-    }
-
-
-def make_tweet_string(username, num_tweets):
-    """Produce a valid string of generated tweets"""
-    data = [tweet.text for tweet in get_all_tweets(username)]
-    tweet_string = "<br/>#############<br/>".join(generate(data, num_tweets))
-    return tweet_string
+def make_markov_model(tweets):
+    """Wrapper around making Markov Chain"""
+    return markovify.Text(" ".join(tweets))
 
 if __name__ == '__main__':
     make_tweet_string("realDonaldTrump", 10)
