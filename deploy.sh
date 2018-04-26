6286612a4bb094d098894173820fdfaa4c75cd96
Matt Lewis <domattthings@gmail.com>
2018-04-26 19:17:05 -0400

    Virtual env

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-
+pip install virutalenv
+virtualenv twit_ci
 cd $TRAVIS_BUILD_DIR
 source twit_ci/bin/activate
+pip install -r requirements.txt
 pip install zappa
 zappa update production
