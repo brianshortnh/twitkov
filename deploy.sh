94f1407ad0a279cb7666b1d3712e749258dcdcab
Matt Lewis <domattthings@gmail.com>
2018-05-27 16:08:53 -0400

    Use python3.6 during deployment

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-pip install virutalenv
-virtualenv twit_ci
+pip install --upgrade virutalenv
+virtualenv --python=python3.6 twit_ci
 cd $TRAVIS_BUILD_DIR
 source twit_ci/bin/activate
 pip install -r requirements.txt
